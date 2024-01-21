4ce823341d094112fce0b7bb990f322c62def522
amitsharma1511 <amitsharma1511@yahoo.com>
2024-01-21 23:32:40 +0530

    updated css

--- src/ui/Pre.jsx
+++ src/ui/Pre.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Pre = styled.pre`
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   font-size: 1.1rem;
+  white-space: pre-wrap; /* Handles word wrapping */
 `;
 
 export default Pre;
